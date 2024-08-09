8ca61d5fcc99c8e09d82749494a5e05fdc3d98fa
samueljwwright <samueljwwright@gmail.com>
2024-08-09 13:27:46 +0100

    Add project files.

---
+++ MSlearn01/Data/MSlearn01Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MSlearn01.Data
+{
+    public class MSlearn01Context : DbContext
+    {
+        public MSlearn01Context (DbContextOptions<MSlearn01Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MSlearn01/Views/HelloWorld/Welcome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MSlearn01.Views.HelloWorld
+{
+    public class WelcomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
